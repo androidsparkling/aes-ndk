6c7c32bc7e9516bf230a31b587619cd56d10185f
zoulong <839169106@qq.com>
2019-11-29 21:01:13 +0800

    。

--- README.mk
+++ README.mk
@@ -1,2 +1,2 @@
 使用C++实现AES加密，android通过JNI调用C++中的加密方法，可以将RES的密钥藏在到C++中
-![image]("https://raw.githubusercontent.com/ray-tianfeng/AES-NDK/master/demonstration.gif")
+![image](https://raw.githubusercontent.com/ray-tianfeng/AES-NDK/master/demonstration.gif)
